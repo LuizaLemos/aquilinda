d01fdb1a53a2a0939bf7850b92c236240086db56
Aluno <Aluno@PVH-LAB-TESTE01>
2018-10-03 12:24:06 -0400

    primeira commit luiza

---
+++ src/aula0310/Aula0310.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula0310;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Aula0310 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hoje é um dia happy");        
+
+
+
+// TODO code application logic here
+    }
+    
+}
